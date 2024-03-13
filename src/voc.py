a815c0286ac224fe025067603f21685fd5c391c8
Steve <stevez@playgo.ai>
2024-03-14 00:21:48 +0800

    change voc.py

--- src/voc.py
+++ src/voc.py
@@ -1,53 +1,75 @@
 #######################
-# voc
+# constants
 #######################
 
-# singleton (one word)
-x_help = "help"
+# singleton
+x_help = 'help'
+x_edit_mode = 'edit mode'
+x_reset_game = 'reset game'
+
+# object and verb
+x_change_background = 'change background'
+x_change_position = 'change position'
+x_change_skin = 'change skin'
+
+# objects
+
+x_snake = 'snake'
+x_prop = 'prop' # props
+x_obstacle = 'obstacle'
+x_prop_and_obstacle = f'{x_prop} and {x_obstacle}'
+x_weapon = 'weapon'
+x_food = 'food'
+x_enemy = 'enemy'
+# x_object = 'object'
+x_snake_size = 'snake size'
+x_prop_size = f'{x_prop} size'
+x_map_size = 'map size'
+x_mode = 'mode' # game mode
+x_music = 'music'
+# x_prop_speed = f'{x_prop} speed'
+x_snake_speed = f'{x_snake} speed'
+
+x_magnet = 'magnet'
+x_bomb = 'bomb'
+x_sword = 'sword'
+x_stick = 'stick'
+x_gun = 'gun'
+
+x_wood = 'wood'
+x_steel = 'steel'
+x_iron = 'iron'
+
+x_speed = 'speed'
+x_timer = 'timer'
+x_winning_condition = 'winning condition'
+x_survival_winning_condition = 'survival winning condition'
+x_enemy_attack_around = f'{x_enemy} attack around'
+x_obstacle_attack_around = f'{x_obstacle} attack around'
+x_snake_self_damage = f'{x_snake} self damage'
+x_enemy_espawn = f"{x_enemy} respawn"
 
 # verb
-x_list = "list"
-x_reset = "reset"
-x_set = "set"
-x_add = "add"
-x_delete = "delete"
-x_enable = "enable" # switch-on and off
-x_disable = "disable" # switch-on and off
-x_increase = "increase"
-x_decrease = "decrease"
-x_change = "change"
-x_drop = "drop" # spawn/drop
-x_custmize = "custmize"
+x_set = 'set'
+x_add = 'add'
+x_delete = 'delete'
+x_enable = 'enable'
+x_disable = 'disable'
+x_list = 'list'
+x_increase = 'increase'
+x_decrease = 'decrease'
+x_replace = 'replace'
+x_patrol = 'patrol'
+x_last = 'last' # eg: timer lasts for 2 minutes
 
-
-# noun
-x_mode = "mode"  # game mode, not modes, in voc, we prefer single over plurs
-x_fruit = "fruit"  # objects/items/elements/fruits
-x_background = "background"
-x_music = "music"
-x_image = "image"
-x_game = "game"
-x_winning_condition = "winning condition"
-x_rule = "rule"  # means formula or blueprint, etc
-x_prop = "prop"  # powerups/buffs/boosts
-x_death_line = "death line"  # "game over line"/ceiling
-x_bomb = "bomb"
-x_freeze = "freeze"
-x_lightning = "lightning"
-x_swap_card = "swap card"
-# x_object = "object"
-x_timer = "timer"
+x_start = 'start'
+x_end = 'end'
 
 # properties
-x_skin = "skin"  # images/sprites/skin/pictures/art
-# x_quantity = "quantity"
-x_color = "color"
-x_odd = "odd"
-x_value = "value"
+x_quantity = 'quantity'
+x_color = 'color'
 # x_type = 'type'
-x_size = "size"
-x_args = "args"
-x_power = "power"
-# x_unit = "unit"
-x_multiplier = "multiplier"
-x_muliple = "multiple"
+x_size = 'size'
+x_args = 'args'
+x_unit = 'unit'
+x_multiplier = 'multiplier'
