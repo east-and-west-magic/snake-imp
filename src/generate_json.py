ad2a556bb32dcea854fe60d6635b6addfa5a8b1a
anan <2577759001@qq.com>
2024-03-14 15:17:13 +0800

    enemy destroyable

--- src/generate_json.py
+++ src/generate_json.py
@@ -1,3 +1,77 @@
 from .util import dump
 from .voc import *
 import pytest
+
+
+@pytest.mark.skip
+@dump
+def test_list_mode(train_generated: dict):
+    ###########################################
+    # list mode
+    ###########################################
+    for s in [
+        "show mode list",
+        "what modes do you support?",
+    ]:
+        xxx = {
+            s: {
+                "result": [
+                    x_list,
+                    x_mode,
+                    {},
+                ],
+                "desc": [
+                    "list mode",
+                ],
+            },
+        }
+        train_generated |= xxx
+
+
+@pytest.mark.skip
+@dump
+def test_list_mode2(train_generated: dict):
+    ###########################################
+    # list mode
+    ###########################################
+    for s in [
+        "mode list",
+        "list modes",
+    ]:
+        xxx = {
+            s: {
+                "result": [
+                    x_list,
+                    x_mode,
+                    {},
+                ],
+                "desc": [
+                    "list mode",
+                ],
+            },
+        }
+        train_generated |= xxx
+
+
+@dump
+def test_enemy_destroyable(train_generated: dict):
+    ###########################################
+    # enemy destroyable
+    ###########################################
+    for s in [
+        "make enemy indestructible",
+        "make enemy unbreakable",
+    ]:
+        xxx = {
+            s: {
+                "result": [
+                    x_disable,
+                    x_enemy,
+                    {x_args: "destroyable"}
+                ],
+                "desc": [
+                    "enable/disable obstacles/enemies destroyable",
+                ],
+            },
+        }
+        train_generated |= xxx
