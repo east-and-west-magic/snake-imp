4d6fc6fd10e11c4e23f8dbd1b57ac59e3f083a39
Beracles <zhoujiang45@qq.com>
2024-03-15 19:21:16 +0800

    make weapons stationary

--- src/generate_json.py
+++ src/generate_json.py
@@ -2,3 +2,28 @@ from .util import dump
 from .voc import *
 import pytest
 
+@dump
+def test_make_weapon_stationary(train_generated: dict):
+    ###########################################
+    # make weapon stationary
+    ###########################################
+    for s in [
+        "make weapons stationary",
+        "make the weapons stationary",
+        "stop weapons from moving",
+        "let weapons stand still",
+    ]:
+        xxx = {
+            s: {
+                "result": [
+                    x_set,
+                    x_weapon,
+                    x_speed,
+                    {x_args: 0},
+                ],
+                "desc": [
+                    "make weapons stationary",
+                ],
+            },
+        }
+        train_generated |= xxx
