3ed5b0a82fc089f8303e299aba8c1a72777c1108
Steve <stevez@playgo.ai>
2024-03-15 17:22:24 +0800

    clean up

--- src/generate_json.py
+++ src/generate_json.py
@@ -3,54 +3,6 @@ from .voc import *
 
 
 @dump
-def test_list_mode(train_generated: dict):
-    ###########################################
-    # list mode
-    ###########################################
-    for s in [
-        "show mode list",
-        "what modes do you support?",
-    ]:
-        xxx = {
-            s: {
-                "result": [
-                    x_list,
-                    x_mode,
-                    {},
-                ],
-                "desc": [
-                    "list mode",
-                ],
-            },
-        }
-        train_generated |= xxx
-
-
-@dump
-def test_list_mode2(train_generated: dict):
-    ###########################################
-    # list mode
-    ###########################################
-    for s in [
-        "mode list",
-        "list modes",
-    ]:
-        xxx = {
-            s: {
-                "result": [
-                    x_list,
-                    x_mode,
-                    {},
-                ],
-                "desc": [
-                    "list mode",
-                ],
-            },
-        }
-        train_generated |= xxx
-
-
-@dump
 def test_play_game(train_generated: dict):
     ###########################################
     # play game
