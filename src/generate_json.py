4046e4111c83f8b3088f7b425e4398a80ad51089
Steve <stevez@playgo.ai>
2024-03-15 19:08:56 +0800

    clean up

--- src/generate_json.py
+++ src/generate_json.py
@@ -3,56 +3,6 @@ from .voc import *
 import pytest
 
 
-@pytest.mark.skip
-@dump
-def test_list_mode(train_generated: dict):
-    ###########################################
-    # list mode
-    ###########################################
-    for s in [
-        "show mode list",
-        "what modes do you support?",
-    ]:
-        xxx = {
-            s: {
-                "result": [
-                    x_list,
-                    x_mode,
-                    {},
-                ],
-                "desc": [
-                    "list mode",
-                ],
-            },
-        }
-        train_generated |= xxx
-
-
-@pytest.mark.skip
-@dump
-def test_list_mode2(train_generated: dict):
-    ###########################################
-    # list mode
-    ###########################################
-    for s in [
-        "mode list",
-        "list modes",
-    ]:
-        xxx = {
-            s: {
-                "result": [
-                    x_list,
-                    x_mode,
-                    {},
-                ],
-                "desc": [
-                    "list mode",
-                ],
-            },
-        }
-        train_generated |= xxx
-
-
 @dump
 def test_change_skin(train_generated: dict):
     ###########################################
