2a789a3892077804950af6ba38e31975f722f71e
Steve <stevez@playgo.ai>
2024-03-15 19:29:58 +0800

    clean up

--- src/generate_json.py
+++ src/generate_json.py
@@ -2,28 +2,3 @@ from .util import dump
 from .voc import *
 import pytest
 
-
-@dump
-def test_change_skin(train_generated: dict):
-    ###########################################
-    # change skin
-    ###########################################
-    for weapon in [
-        "gun", "sword", "club", "bomb",
-    ]:
-        for s in [
-            f"Change image for {weapon}",
-            f"change picture for {weapon}",
-        ]:
-            xxx = {
-                s: {
-                    "result": [
-                        x_change_skin,
-                        {x_args: x_weapon},
-                    ],
-                    "desc": [
-                        "change skin",
-                    ],
-                },
-            }
-            train_generated |= xxx
